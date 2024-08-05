d10d0d5b8245f637277181ab7a4a6c2dace1c4df
rikeda-cloud <longsichitian648@gmail.com>
2024-08-05 14:16:48 +0900

    [add] redisをwordpressと連携

--- inception/srcs/requirements/wordpress/tools/init_wordpress.sh
+++ inception/srcs/requirements/wordpress/tools/init_wordpress.sh
@@ -27,3 +27,13 @@ wp core install \
 
 # WordPressのスキームの設定
 wp theme activate twentytwentytwo --allow-root
+
+# redis用の設定をconfigファイルに追記
+wp config set WP_CACHE "true" --allow-root
+wp config set WP_REDIS_HOST "redis" --allow-root
+wp config set WP_REDIS_PORT "6379" --allow-root
+wp config set WP_REDIS_DATABASE "0" --allow-root
+
+# redisプラグインをインストール
+wp plugin install redis-cache --path=/var/www/html/wordpress --activate --allow-root
+wp redis enable --path=/var/www/html/wordpress --allow-root
