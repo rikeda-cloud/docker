1a88be6defca4f3aaefc821f0470309e2686ce17
rikeda <110877359+rikeda-cloud@users.noreply.github.com>
2024-08-05 11:28:21 +0900

    Rust (#1)
    * [add] rustでWebアプリケーションをビルドし、systemd経由で起動
    
    * [update] nginxにヘルスチェック機構を導入

---
+++ inception/srcs/requirements/rust/tools/main.rs
@@ -0,0 +1,12 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root_handler));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root_handler() -> String {
+    "<h1>Hello World!!!</h1>".to_string()
+}
