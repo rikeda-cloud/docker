6b923f558d7d26938437398ea20b037623a4f2a3
rikeda-cloud <longsichitian648@gmail.com>
2024-08-11 14:41:28 +0900

    [update] rustサーバのlistenポートを3000 -> 4000

--- inception/srcs/requirements/rust/tools/main.rs
+++ inception/srcs/requirements/rust/tools/main.rs
@@ -3,7 +3,7 @@ use axum::{routing::get, Router};
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(root_handler));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:4000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
