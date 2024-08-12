1e0f32e3b866e605f2e793e2cebf4c46eec7e218
rikeda-cloud <longsichitian648@gmail.com>
2024-08-12 23:29:06 +0900

    [update] Wordpressのconfigファイルの作成をDockerfileで実行

--- inception/srcs/requirements/wordpress/tools/init_wordpress.sh
+++ inception/srcs/requirements/wordpress/tools/init_wordpress.sh
@@ -2,16 +2,6 @@
 
 cd /var/www/html/wordpress
 
-# configファイルの作成
-wp core config \
-	--dbname=wordpress \
-	--dbuser=wordpress \
-	--dbpass=wordpress \
-	--dbhost=mariadb \
-	--dbprefix=wp_ \
-	--skip-check \
-	--allow-root
-
 # WordPressの初期化
 wp core install \
 	--url="https://rikeda.42.fr" \
@@ -25,12 +15,9 @@ wp core install \
 # WordPressのスキームの設定
 wp theme activate twentytwentytwo --allow-root
 
-# redis用の設定をconfigファイルに追記
-wp config set WP_CACHE "true" --allow-root
-wp config set WP_REDIS_HOST "redis" --allow-root
-wp config set WP_REDIS_PORT "6379" --allow-root
-wp config set WP_REDIS_DATABASE "0" --allow-root
-
 # redisプラグインをインストール
-wp plugin install redis-cache --path=/var/www/html/wordpress --activate --allow-root
 wp redis enable --path=/var/www/html/wordpress --allow-root
+wp plugin install redis-cache --path=/var/www/html/wordpress --activate --allow-root
+
+# userを追加
+wp user create nono nono@gmail.com --role=author --user_pass=password --display_name=NONO
