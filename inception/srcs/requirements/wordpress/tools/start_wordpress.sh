616f13c81dd94707e06bc1dfb529bd497a0075c4
rikeda-cloud <longsichitian648@gmail.com>
2024-08-13 17:59:27 +0900

    [update] use .env to wordpress, nginx

--- inception/srcs/requirements/wordpress/tools/start_wordpress.sh
+++ inception/srcs/requirements/wordpress/tools/start_wordpress.sh
@@ -7,11 +7,13 @@ mkdir -p /var/www/html && \
 	wp core download --path=/var/www/html/wordpress --locale=ja --allow-root && \
 	chmod -R 777 /var/www/html/wordpress
 
+export > /root/result_export.txt
+
 # configファイルの作成
 wp core config \
-	--dbname=wordpress \
-	--dbuser=wordpress \
-	--dbpass=wordpress \
+	--dbname=${WORDPRESS_DB_NAME} \
+	--dbuser=${MARIADB_USER} \
+	--dbpass=${MARIADB_PASSWORD} \
 	--dbhost=mariadb \
 	--dbprefix=wp_ \
 	--skip-check \
@@ -19,16 +21,21 @@ wp core config \
 
 # WordPressの初期化
 wp core install \
-	--url="https://rikeda.42.fr" \
-	--title="rikeda-cloud" \
-	--admin_user="rikeda" \
-	--admin_password="rikeda-cloud" \
-	--admin_email="longsichitian648@gmail.com" \
+	--url=https://${DOMAIN_NAME} \
+	--title=${WORDPRESS_TITLE} \
+	--admin_user=${WORDPRESS_ADMIN_USER} \
+	--admin_password=${WORDPRESS_ADMIN_PASSWORD} \
+	--admin_email=${WORDPRESS_ADMIN_EMAIL} \
 	--skip-email \
 	--allow-root
 
 # admin以外のuserを追加
-wp user create nono nono@gmail.com --role=author --user_pass=password --display_name=NONO --allow-root
+wp user create ${WORDPRESS_NORMAL_USER} \
+	${WORDPRESS_NORMAL_EMAIL} \
+	--role=author \
+	--user_pass=${WORDPRESS_NORMAL_PASSWORD} \
+	--display_name=${WORDPRESS_NORMAL_USER} \
+	--allow-root
 
 # WordPressのスキームの設定
 wp theme activate twentytwentytwo --allow-root
@@ -40,8 +47,13 @@ wp config set WP_CACHE "true" --allow-root && \
 	wp config set WP_REDIS_DATABASE "0" --allow-root
 
 # redisプラグインをインストール
-wp plugin install redis-cache --path=/var/www/html/wordpress --activate --allow-root
-wp redis enable --path=/var/www/html/wordpress --allow-root
+wp plugin install redis-cache \
+	--path=/var/www/html/wordpress \
+	--activate \
+	--allow-root
+wp redis enable \
+	--path=/var/www/html/wordpress \
+	--allow-root
 
 echo finish init wordpress
 
